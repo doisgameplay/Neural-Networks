847b04adca8945c513a430889915fd8c7fbd9a81
MATHEUS VILARINO DE SOUZA PINTO <B435895@fgv.edu.br>
2024-08-24 18:20:06 -0300

    added an matrix sum function

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@ std::vector<std::vector<float>> matrixMultiplication(std::vector<std::vector<flo
 std::vector<std::vector<float>> matrixMultiplication(std::vector<std::vector<float>> A, std::vector<std::vector<float>> B){
     
     int numLines = A.size();
-    int numColums = B[0].size();
+    int numColumns = B[0].size();
     int numMultiplication = A[0].size(); // This value will hold the 'n' of the matrix algorithm multiplication
-    std::vector<std::vector<float>> C(numLines, std::vector<float>(numColums, 0.0f));
+    std::vector<std::vector<float>> C(numLines, std::vector<float>(numColumns, 0.0f));
 
     for(int i = 0;i < numLines; i ++){
         for(int k = 0; k < numLines; k++){
@@ -33,19 +33,33 @@ std::vector<std::vector<float>> matrixMultiplication(std::vector<std::vector<flo
 }
 
 
+std::vector<std::vector<float>> matrixSum(std::vector<std::vector<float>> A, std::vector<std::vector<float>> B);
+std::vector<std::vector<float>> matrixSum(std::vector<std::vector<float>> A, std::vector<std::vector<float>> B){
+    int numLines = A.size();
+    int numColumns = B[0].size();
+    std::vector<std::vector<float>> C(numLines, std::vector<float>(numColumns, 0.0f));
+
+    for(int i = 0; i < numLines; i ++){
+        for(int k = 0; k < numColumns; k++){
+            C[i][k] = A[i][k] + B[i][k];
+        }
+    }
+
+    return C;
+}
+
 
 int main(){
     std::vector<std::vector<float>> A = {
-        {1.0, 2.0, 3.0},
-        {4.0, 5.0, 6.0}
+        {1.0, 2.0},
+        {4.0, 5.0}
     };
 
     std::vector<std::vector<float>> B = {
         {7.0, 8.0},
-        {9.0, 10.0},
-        {11.0, 12.0}
+        {9.0, 10.0}
     };
 
-
+    showMatrix(matrixSum(A,B));
     return 0;
 }
